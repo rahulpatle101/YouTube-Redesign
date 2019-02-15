5d32b50a2f3859e96c139515b310d56f8ebe9df2
Rahul Patle <rahulpatle101@gmail.com>
2019-02-14 19:09:01 -0800

    add static html markup with css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+import HomeView from './components/HomeView';
+import DetailView from './components/DetailView';
 import './App.css';
 
-class App extends Component {
-  render() {
+const App = () => {
     return (
-      <div className="App">
-        <h1>Hello World</h1>
-      </div>
-    );
-  }
+      <Fragment>
+          <Header />
+          <Sidebar />
+          <DetailView />
+      </Fragment>
+    )
 }
 
 export default App;
